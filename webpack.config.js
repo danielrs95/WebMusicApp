8a3be8298f050cf36b28e05326e769cca38f3d7e
Daniel <danielrs9504@gmail.com>
2022-06-26 07:11:26 -0500

    Add typescrypt and fix webpack to see typescript files

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,11 @@ module.exports = {
     port: 3000,
     hot: true,
   },
-  // Rules of how webpack will take our files, complie & bundle them for the browser
+  // Rules of how webpack will take our files, compile & bundle them for the browser
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /nodeModules/,
         use: {
           loader: 'babel-loader',
@@ -27,6 +27,9 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
   },
   plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
