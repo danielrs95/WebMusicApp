6fa7378f53f4af72c6f7a7d911a5963cee67868b
Daniel <danielrs9504@gmail.com>
2022-06-26 13:58:03 -0500

    Add react-hooks plug-in to eslint Fix type warning on index.tsx avoiding container being `null`

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('app');
-const root = createRoot(container);
 
-root.render(<App />);
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
