861397a0ea40d075f1076a9a790e1e1726cdbf21
Daniel <danielrs9504@gmail.com>
2022-06-26 12:29:35 -0500

    Fix intentionally error on index.tsx Add basic rules to test errors from lint-staged and husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+// import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
+
 root.render(<App />);
-// ReactDOM.render(<App />, document.getElementById('root'));
